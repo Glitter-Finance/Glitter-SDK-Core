2654377bd962deef03406e992bca8aa26266b706
Mago2022 <magomed@glitter.finance>
2023-01-11 11:30:19 +0000

    Exporting Solana Wallet and Algorand Wallet

--- src/lib/chains/algorand/index.ts
+++ src/lib/chains/algorand/index.ts
@@ -8,35 +8,25 @@ import {
 
 import { AlgorandAsset, AlgorandAssets } from "./assets";
 
- import {
-    AlgorandConfig,
-    AlgorandAccountsConfig
- } from './config';
- import {
-    AlgorandConnect
- }from './connect';
- import {
-    AlgorandBridgeTxnsV1
- }from './txns/bridge';
- import {
-    AlgorandTxns
- } from './txns/txns';
+import { AlgorandConfig, AlgorandAccountsConfig } from "./config";
+import { AlgorandConnect } from "./connect";
+import { AlgorandBridgeTxnsV1 } from "./txns/bridge";
+import { AlgorandTxns } from "./txns/txns";
 
-export { 
-    AlgorandAccount,
-    AlgorandMSigAccount,
-    AlgorandAccountDetails,
-    AlgorandAccountAsset,
-    AlgorandAccounts,
-    AlgorandAsset,
-    AlgorandAssets,
-    AlgorandConfig,
-    AlgorandAccountsConfig,
-    AlgorandConnect,
-    AlgorandBridgeTxnsV1,
-    AlgorandTxns
-     };
+import { AlgorandWallet } from "./wallet";
 
-
-
-     
+export {
+  AlgorandAccount,
+  AlgorandMSigAccount,
+  AlgorandAccountDetails,
+  AlgorandAccountAsset,
+  AlgorandAccounts,
+  AlgorandAsset,
+  AlgorandAssets,
+  AlgorandConfig,
+  AlgorandAccountsConfig,
+  AlgorandConnect,
+  AlgorandBridgeTxnsV1,
+  AlgorandTxns,
+  AlgorandWallet,
+};
--- src/lib/chains/solana/index.ts
+++ src/lib/chains/solana/index.ts
@@ -1,35 +1,26 @@
+import { SolanaBridgeTxnsV1 } from "./txns/bridge";
+import { SolanaTxns } from "./txns/txns";
 import {
-    SolanaBridgeTxnsV1
-} from './txns/bridge';
-import{
-    SolanaTxns
-} from './txns/txns';
-import {
-    SolanaAccounts,
-    SolanaAccountDetails,
-    SolanaTokenAccount,
-    SolanaAccount
-} from './accounts';
-import {
-    SolanaAssets,
-    SolanaAsset
-} from './assets';
-import{
-    SolanaConfig
-} from './config';
-import {
-    SolanaConnect
-} from './connect';
+  SolanaAccounts,
+  SolanaAccountDetails,
+  SolanaTokenAccount,
+  SolanaAccount,
+} from "./accounts";
+import { SolanaAssets, SolanaAsset } from "./assets";
+import { SolanaConfig } from "./config";
+import { SolanaConnect } from "./connect";
+import { SolanaWallet } from "./wallet";
 
-export{
-    SolanaBridgeTxnsV1,
-    SolanaTxns,
-    SolanaAccounts,
-    SolanaAccountDetails,
-    SolanaTokenAccount,
-    SolanaAccount,
-    SolanaAssets,
-    SolanaAsset,
-    SolanaConfig,
-    SolanaConnect
-}
+export {
+  SolanaBridgeTxnsV1,
+  SolanaTxns,
+  SolanaAccounts,
+  SolanaAccountDetails,
+  SolanaTokenAccount,
+  SolanaAccount,
+  SolanaAssets,
+  SolanaAsset,
+  SolanaConfig,
+  SolanaConnect,
+  SolanaWallet,
+};
