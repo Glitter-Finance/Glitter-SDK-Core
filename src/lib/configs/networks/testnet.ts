a20b365a581726ccb0d6740fb07c13d74d2a1d1e
Saad Ahmed <saadahm.siddiqui@gmail.com>
2023-01-20 16:40:23 +0500

    Algorand and solana

--- src/lib/configs/networks/testnet.ts
+++ src/lib/configs/networks/testnet.ts
@@ -18,8 +18,9 @@ export const BridgeTestnet: GlitterBridgeConfig = {
       feeReceiver: "A2GPNMIWXZDD3O3MP5UFQL6TKAZPBJEDZYHMFFITIAJZXLQH37SJZUWSZQ",
       multiSig1: "JPDV3CKFABIXDVH36E7ZBVJ2NC2EQJIBEHCKYTWVC4RDDOHHOPSBWH3QFY",
       multiSig2: "DFFTYAB6MWMRTZGHL2GAP7TMK7OUGHDD2AACSO7LXSZ7SY2VLO3OEOJBQU",
-      usdcReceiver: "",
-      usdcDeposit: "",
+      usdcReceiver:
+        "JJWJKLUIMX3THW3CT6ZQMBRPBQ34MUFNS7PFV4YU7CCLS2KPUBOS7XFJDY",
+      usdcDeposit: "YHAQ76NAMVBY3VC3MUF4AEMI3NKZBI6CQFLMUBN4NOET4QLBMZ7Z4Q4T4E",
       bridge: "XJQ25THCV734QIUZARPZGG3NPRFZXTIIU77JSJBT23TJMGL3FXJWVR57OQ",
       asaVault: "U4A3YARBVMT7PORTC3OWXNC75BMGF6TCHFOQY4ZSIIECC5RW25SVKNKV3U",
       algoVault: "R7VCOR74LCUIFH5WKCCMZOS7ADLSDBQJ42YURFPDT3VGYTVNBNG7AIYTCQ",
@@ -39,7 +40,7 @@ export const BridgeTestnet: GlitterBridgeConfig = {
       {
         network: "algorand",
         symbol: "USDC",
-        address: undefined,
+        address: 10458941,
         decimals: 6,
         min_transfer: 5,
         fee_divisor: 200,
@@ -61,15 +62,15 @@ export const BridgeTestnet: GlitterBridgeConfig = {
     ],
   },
   solana: {
-    name: "mainnet-beta",
-    server: "https://api.mainnet-beta.solana.com",
+    name: "devnet",
+    server: "https://api.devnet.solana.com",
     accounts: {
       bridgeProgram: "GLittnj1E7PtSF5thj6nYgjtMvobyBuZZMuoemXpnv3G",
       vestingProgram: "EMkD74T2spV3A71qfY5PNqVNrNrpbFcdwMF2TerRMr9n",
       owner: "hY5PXHYm58H5KtJW4GrtegxXnpMruoX3LLP6CufHoHj",
       usdcReceiver: "Av47VxT8GpGXHYc3aG7fKddgZjCuZEb5yF3BCaXyE7wu",
-      usdcDeposit: "C4ZaLj7a5CPQ4r7TwXJc8cAyubXUx9XcN2zEHoP7peED",
-      memoProgram: "",
+      usdcDeposit: "8Cb6eKCiowqsfYoLeaQf9voTHv1nV6rKjBvMQwLEGoDJ",
+      memoProgram: "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr",
     },
     tokens: [
       {
@@ -97,7 +98,7 @@ export const BridgeTestnet: GlitterBridgeConfig = {
       {
         network: "solana",
         symbol: "USDC",
-        address: "",
+        address: "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU",
         decimals: 6,
         min_transfer: 1,
         fee_divisor: 200,
