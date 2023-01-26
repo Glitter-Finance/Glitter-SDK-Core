251382e005371af2d55565862df8b75dba07f837
Andrew Carl <andrewc@glitter.finance>
2023-01-26 12:24:23 -0500

    Added timestamp to partialbridgetxn

--- src/lib/common/transactions/transactions.ts
+++ src/lib/common/transactions/transactions.ts
@@ -18,6 +18,7 @@ export enum ChainStatus{
 
 export type PartialBridgeTxn ={
     txnID:string, 
+    txnTimestamp:Date,
     txnType:TransactionType, 
     chainStatus?:ChainStatus|null,
     network?:string|null,
